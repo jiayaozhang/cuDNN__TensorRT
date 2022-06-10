061e0c8a37fe8bf147c30e2057ff0db580e1754e
jiayaozhang <jiayao.zhang@kaust.edu.sa>
2022-06-10 16:04:04 +0800

    tensorRT

--- chapter5/作业第五章/Bertmodel 2 ONNX.py
+++ chapter5/作业第五章/Bertmodel 2 ONNX.py
@@ -1,5 +1,3 @@
-Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
 import torch
 from torch.nn import functional as F
 import numpy as np
